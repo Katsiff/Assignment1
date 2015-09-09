2a147266683d28c576e80cd897922557a5a70165
Katrina Siffrinn <SiffrinnK@my.wccc.edu>
2015-09-09 17:32:33 -0400

    Java initial commit Assignment 1

---
+++ assignment1countingword/Assignment1CountingWord.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment1countingword;
+
+/**
+ *
+ * @author Katrina
+ */
+public class Assignment1CountingWord {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String helloWord = "Hello";
+        char firstChar = helloWord.charAt(0);
+        char secondChar = helloWord.charAt(1); 
+        char thirdChar = helloWord.charAt(2);
+        char fourthChar = helloWord.charAt(3);
+        char fifthChar = helloWord.charAt(4);
+        int stringSize = helloWord.length();
+        
+        System.out.println(stringSize);
+        System.out.println((int)firstChar);
+        System.out.println((int)secondChar);
+        System.out.println((int)thirdChar);
+        System.out.println((int)fourthChar);
+        System.out.println((int)fifthChar);
+        System.out.println (firstChar + secondChar + thirdChar + fourthChar +fifthChar);
+        
+        
+        
+                
+                
+                
+                
+    }
+  
+    
+            
+            
+}
